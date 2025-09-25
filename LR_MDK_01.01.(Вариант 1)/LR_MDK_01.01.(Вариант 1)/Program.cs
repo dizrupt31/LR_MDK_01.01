739bc56e6f7a1ea9fc3dbe0603f9fe6535d34735
dizrupt31 <158052322+dizrupt31@users.noreply.github.com>
2025-09-25 19:40:38 +0300

    Считываем размер массива,создаем его и заполняем элементами

---
+++ LR_MDK_01.01.(Вариант 1)/LR_MDK_01.01.(Вариант 1)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR_MDK_01._01._Вариант_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов в массиве:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            Console.WriteLine("Введите элементы массива:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Элемент {i + 1}: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
+        
+        
+ 
