ec9652af9ce7d57f0abc303ec15a76d304e87a5d
dizrupt31 <158052322+dizrupt31@users.noreply.github.com>
2025-09-25 19:45:00 +0300

    Находим сумму положительных елементов и выводим итоговую сумму

--- LR_MDK_01.01.(Вариант 1)/LR_MDK_01.01.(Вариант 1)/Program.cs
+++ LR_MDK_01.01.(Вариант 1)/LR_MDK_01.01.(Вариант 1)/Program.cs
@@ -12,18 +12,26 @@ namespace LR_MDK_01._01._Вариант_1_
         {
             Console.WriteLine("Введите количество элементов в массиве:");
             int n = Convert.ToInt32(Console.ReadLine());
-
             int[] array = new int[n];
-
             Console.WriteLine("Введите элементы массива:");
             for (int i = 0; i < n; i++)
             {
                 Console.Write($"Элемент {i + 1}: ");
                 array[i] = Convert.ToInt32(Console.ReadLine());
             }
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] > 0)
+                {
+                    sum = sum + array[i]; 
+                }
+            }
+            Console.WriteLine($"Сумма положительных элементов: {sum}");
         }
     }
 }
-        
-        
- 
+
+
+
+
