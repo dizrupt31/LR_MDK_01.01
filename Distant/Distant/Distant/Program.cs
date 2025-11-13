365312e605605bde5cb556cd93e2b50ec42bb141
dizrupt31 <158052322+dizrupt31@users.noreply.github.com>
2025-11-14 00:16:14 +0300

    Добавлена базовая структура данных Student

---
+++ Distant/Distant/Distant/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+public class Student
+{
+    public string Name;
+    public int Course;
+    public double AverageGrade;
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+    static List<Student> FindTop10Students(List<Student> students)
+    {
+        return new List<Student>();
+    }
+}
