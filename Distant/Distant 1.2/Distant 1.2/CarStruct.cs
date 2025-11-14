43f32c297053740ef30a15feddad8c7f17e87645
dizrupt31 <158052322+dizrupt31@users.noreply.github.com>
2025-11-14 23:27:18 +0300

    Создал структуру содержащаю информацию о автомобилях

---
+++ Distant/Distant 1.2/Distant 1.2/CarStruct.cs
@@ -0,0 +1,11 @@
+﻿using System; 
+
+namespace DataStructure
+{
+    public class Car
+    {
+        public string Brand;//марка автомобиля
+        public int Year;   
+        public int Power;    
+    }
+}
