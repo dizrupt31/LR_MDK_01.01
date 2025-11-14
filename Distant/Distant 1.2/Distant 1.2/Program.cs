68056cd14d91e876509efddccd15f37d6a5382c4
dizrupt31 <158052322+dizrupt31@users.noreply.github.com>
2025-11-14 23:37:41 +0300

    Воплотил основную часть программы которая создает 100 машин,а затем реализует фунцию и выводит ТОП-10 по мощности

--- Distant/Distant 1.2/Distant 1.2/Program.cs
+++ Distant/Distant 1.2/Distant 1.2/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DataStructure; 
+using Functions;     
 
-namespace Distant_1._2
+namespace MainProgram
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            List<Car> allCars = new List<Car>(); 
+            Random random = new Random();         
+
+            Console.WriteLine("Создание 100 тестовых автомобилей");
+            for (int i = 1; i <= 100; i++)
+            {
+                Car newCar = new Car();
+                newCar.Brand = "Car Brand " + i;
+                newCar.Year = random.Next(2010, 2025); 
+                newCar.Power = random.Next(90, 601);  
+
+                allCars.Add(newCar);
+            }
+
+            Console.WriteLine("100 автомобилей успешно созданы.");
+            Console.WriteLine("-------------------------------------------");
+            List<Car> topCars = CarFinder.FindTop10Cars(allCars);
+
+            Console.WriteLine("\n ТОП-10 АВТОМОБИЛЕЙ ПО МОЩНОСТИ");
+
+            int rank = 1; 
+            foreach (Car car in topCars)
+            {
+                Console.WriteLine($"{rank}. {car.Brand,-15} (Год: {car.Year}) - Мощность: {car.Power} л.с.");
+                rank++;
+            }
+            Console.WriteLine("-----------------------------------------------");
+
+            Console.ReadKey();
         }
     }
 }
