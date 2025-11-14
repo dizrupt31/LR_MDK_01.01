a302b001576f6ba4c2d26bc91e2c9aa0cd5a707c
dizrupt31 <158052322+dizrupt31@users.noreply.github.com>
2025-11-14 23:31:49 +0300

    Создал функци. реализующую поиск топ 10 машин по их мощности

---
+++ Distant/Distant 1.2/Distant 1.2/Top10CarsFindFunction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic; 
+using System.Linq;                
+using DataStructure;             
+
+namespace Functions
+{
+    public static class CarFinder
+    {
+        public static List<Car> FindTop10Cars(List<Car> allCars)
+        {
+            List<Car> top10 = allCars
+                .OrderByDescending(c => c.Power) 
+                .Take(10)                        
+                .ToList();                       
+
+            return top10;
+        }
+    }
+}
