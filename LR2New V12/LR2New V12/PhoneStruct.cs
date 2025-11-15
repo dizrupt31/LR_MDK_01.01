cb545fdde87f92ef64c3e368632c461ec0a0753a
st56 <skadeshopp@gmail.com>
2025-11-15 13:04:48 +0300

    Создал структуру телефонов и заполнил ее

---
+++ LR2New V12/LR2New V12/PhoneStruct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2New_V12
+{
+    public struct phone
+    {
+        public string brand;
+        public string model;
+        public int NumberOfSales;
+        public int week;
+    }
+}
---
+++ LR2New V12/LR2New V12/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace LR2New_V12
+{
+    static public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<phone> AllPhones = new List<phone>();
+            AllPhones.Add(new phone() { brand = "Samsung", model = "A", NumberOfSales = 5321, week = 1 });
+            AllPhones.Add(new phone() { brand = "Samsung", model = "B", NumberOfSales = 3030, week = 1 });
+            AllPhones.Add(new phone() { brand = "Samsung", model = "C", NumberOfSales = 8745, week = 1 });
+            AllPhones.Add(new phone() { brand = "Samsung", model = "A", NumberOfSales = 8642, week = 2 });
+            AllPhones.Add(new phone() { brand = "Samsung", model = "B", NumberOfSales = 6070, week = 2 });
+            AllPhones.Add(new phone() { brand = "Samsung", model = "C", NumberOfSales = 14430, week = 2 });
+            AllPhones.Add(new phone() { brand = "Iphone", model = "A", NumberOfSales = 44283, week = 1 });
+            AllPhones.Add(new phone() { brand = "Iphone", model = "B", NumberOfSales = 20425, week = 1 });
+            AllPhones.Add(new phone() { brand = "Iphone", model = "C", NumberOfSales = 64423, week = 1 });
+            AllPhones.Add(new phone() { brand = "Iphone", model = "A", NumberOfSales = 124653, week = 2 });
+            AllPhones.Add(new phone() { brand = "Iphone", model = "B", NumberOfSales = 96000, week = 2 });
+            AllPhones.Add(new phone() { brand = "Iphone", model = "C", NumberOfSales = 141899, week = 2 });
+            AllPhones.Add(new phone() { brand = "Huawei", model = "A", NumberOfSales = 4283, week = 1 });
+            AllPhones.Add(new phone() { brand = "Huawei", model = "B", NumberOfSales = 2435, week = 1 });
+            AllPhones.Add(new phone() { brand = "Huawei", model = "C", NumberOfSales = 6423, week = 1 });
+            AllPhones.Add(new phone() { brand = "Huawei", model = "A", NumberOfSales = 4653, week = 2 });
+            AllPhones.Add(new phone() { brand = "Huawei", model = "B", NumberOfSales = 3210, week = 2 });
+            AllPhones.Add(new phone() { brand = "Huawei", model = "C", NumberOfSales = 1287, week = 2 });
+            Console.WriteLine ("Введите марку устройства");
+            string marka = Console.ReadLine();
+        }
+    }
+}
