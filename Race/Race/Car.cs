b068b7b4fbfe68d59483c61a712a75c034d8cbe4
Anon1m4k <kirilvalera2@gmail.com>
2025-11-22 11:36:22 +0300

    Почти готовые машинки

---
+++ Race/Race/Car.cs
@@ -0,0 +1,32 @@
+﻿using System.Management.Instrumentation;
+
+namespace Race
+{
+    public class Car
+    {
+        private string model_;
+        private int speed_;
+
+        public void SetModel(string model)
+        {
+            model_ = model;
+        }
+        public string GetModel()
+        {
+            return model_;
+        }
+        public void SetSpeed(int speed)
+        {
+            speed_ = speed;
+        }
+        public int GetSpeed()
+        {
+            return speed_;
+        }
+
+
+    }
+}
+
+
+
---
+++ Race/Race/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Race
+{
+    class Program
+    {
+        static public void PrintModel(Car car)
+        {
+            Console.WriteLine(car.GetModel());
+        }
+        static public void LadaVSMers(List<Car> cars)
+        {
+            int time = 0;
+            int finish = 248;
+            while (true)
+            {
+                foreach (Car car in cars)
+                {
+                    int SpaceSpeed = car.GetSpeed() / 50;
+                    for (int i = 0; i < SpaceSpeed;)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(car.GetModel());
+                    Console.WriteLine();
+                    if (SpaceSpeed < finish)
+                    {
+                        Console.WriteLine("Победитель - " + car.GetModel());
+                    }
+
+                  
+                }
+                time++;
+                Thread.Sleep(1000);
+                    Console.Clear();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.SetModel("Лада");
+            Console.WriteLine(car1.GetModel());
+            car1.SetSpeed(150);
+            Car car2 = new Car();
+            car2.SetModel("Мерседес");
+            Console.WriteLine(car1.GetModel());
+            car2.SetSpeed(500);
+            PrintModel(car2);
+            List<Car> Cars = new List<Car>();
+            Cars.Add(car1);
+            Cars.Add(car2);
+            LadaVSMers(Cars);
+        } 
+    }
+}
