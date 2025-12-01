04eea8be465438d06357d335c5aef9f56abb7121
dizrupt31 <158052322+dizrupt31@users.noreply.github.com>
2025-12-02 01:33:35 +0300

    Создание массива с произвольным размером и заполнение его данными

---
+++ LR1 задание 1/LR1 задание 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1_задание_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Бородин Вариант_2 Задание_1
+            Console.WriteLine("Введите размер массива: ");
+            int lengthArray = Convert.ToInt32(Console.ReadLine());
+            int[] Array = new int[lengthArray];
+            Console.WriteLine("Введите элементы массива");
+            for (int i = 0; i < lengthArray; i++)
+            {
+                Array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
