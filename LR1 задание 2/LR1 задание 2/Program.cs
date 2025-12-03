68da005001467eaeddffe3a35879a0f86908d170
dizrupt31 <158052322+dizrupt31@users.noreply.github.com>
2025-12-03 22:03:26 +0300

    Инициализация и заполнение массива последовательными числами

---
+++ LR1 задание 2/LR1 задание 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1_задание_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[1990];
+            int initValue = 10;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = initValue;
+                ++initValue;
+            }
+            List<int> result = new List<int>();
+        }
+    }
+}
