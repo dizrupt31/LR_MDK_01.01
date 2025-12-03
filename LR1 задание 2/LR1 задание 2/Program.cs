b4b6cc49d40cbe6278319567546dde820445f74e
dizrupt31 <158052322+dizrupt31@users.noreply.github.com>
2025-12-03 23:41:06 +0300

    Реализован алгоритм поиска и сбора простых чисел

--- LR1 задание 2/LR1 задание 2/Program.cs
+++ LR1 задание 2/LR1 задание 2/Program.cs
@@ -18,6 +18,31 @@ namespace LR1_задание_2
                 ++initValue;
             }
             List<int> result = new List<int>();
+            foreach (int element in array)
+            {
+                bool isSimple = true;
+
+                if (element < 2)
+                {
+                    isSimple = false;
+                }
+                else
+                {
+                    for (int divider = 2; divider * divider <= element; divider++)
+                    {
+                        if (element % divider == 0)
+                        {
+                            isSimple = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (isSimple)
+                {
+                    result.Add(element);
+                }
+            }
         }
     }
-}
+    }
