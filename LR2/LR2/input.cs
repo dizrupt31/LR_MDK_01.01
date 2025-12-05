521f0694a9e64d7faa810efb5fe1f0394d9797ef
dizrupt31 <158052322+dizrupt31@users.noreply.github.com>
2025-12-05 21:33:57 +0300

    Добавлена структура данных и инициализация игр по жанрам

---
+++ LR2/LR2/input.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    public class Input
+    {
+        static public (List<string>[], List<int>[]) InputGamesByGenre() 
+        {
+            List<string>[] gamesByGenre = new List<string>[4] {
+        new List<string>(), 
+        new List<string>(), 
+        new List<string>(), 
+        new List<string>() 
+        };
+            List<int>[] counts = new List<int>[4] {
+        new List<int>(), 
+        new List<int>(),
+        new List<int>(), 
+        new List<int>() 
+        };
+            gamesByGenre[0].Add("The Witcher 3: Wild Hunt"); 
+            counts[0].Add(1500000);
+            gamesByGenre[0].Add("Cyberpunk 2077");
+            counts[0].Add(1200000);
+            gamesByGenre[0].Add("Skyrim");
+            counts[0].Add(1800000);
+            gamesByGenre[0].Add("Dark Souls III");
+            counts[0].Add(900000);
+
+            gamesByGenre[1].Add("Call of Duty: Modern Warfare"); 
+            counts[1].Add(2000000);
+            gamesByGenre[1].Add("Counter-Strike 2");
+            counts[1].Add(2500000);
+            gamesByGenre[1].Add("DOOM Eternal");
+            counts[1].Add(800000);
+            gamesByGenre[1].Add("Battlefield 6");
+            counts[1].Add(600000);
+
+            gamesByGenre[2].Add("Grand Theft Auto V"); 
+            counts[2].Add(3000000);
+            gamesByGenre[2].Add("Red Dead Redemption 2");
+            counts[2].Add(1600000);
+            gamesByGenre[2].Add("God of War");
+            counts[2].Add(1400000);
+            gamesByGenre[2].Add("Assassin's Creed Valhalla");
+            counts[2].Add(1100000);
+
+            gamesByGenre[3].Add("Minecraft"); 
+            counts[3].Add(4000000);
+            gamesByGenre[3].Add("Terraria");
+            counts[3].Add(700000);
+            gamesByGenre[3].Add("Roblox");
+            counts[3].Add(3500000);
+            gamesByGenre[3].Add("Garry's Mod");
+            counts[3].Add(500000);
+
+            return (gamesByGenre, counts);
+        }
+    }
+}
