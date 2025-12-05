37a482f25d4fa0ab4bcfa04a0342bd458a5c660e
dizrupt31 <158052322+dizrupt31@users.noreply.github.com>
2025-12-05 21:39:36 +0300

    Реализован класс Search для поиска жанров и игр

---
+++ LR2/LR2/Search.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    public class Search
+    {
+        static public int FindIndexGenre(string userQuery, string[] genres) // поиск индекса жанра
+        {
+            for (int index = 0; index < genres.Length; ++index)
+            {
+                string valueGenre = genres[index].ToLower();
+                if (valueGenre == userQuery.ToLower())
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+        static public (List<string>, List<int>) FindAllGamesByGenre(int indexGenre, // получение всех игр определенного жанра
+                                                                   List<string>[] allGames,
+                                                                   List<int>[] counts)
+        {
+            List<string> gamesByGenre = new List<string>();
+            List<int> countsByGenre = new List<int>();
+            gamesByGenre = allGames[indexGenre];
+            countsByGenre = counts[indexGenre];
+            return (gamesByGenre, countsByGenre);
+        }
+    }
+}
