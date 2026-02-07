733e2734e51c72c0afb801b17c90b2065770b4aa
Anon1m4k <kirilvalera2@gmail.com>
2026-02-07 12:59:53 +0300

    Сделал первую часть лабораторной работы номер 3

---
+++ LR 3/LR 3/MainForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace LR_3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.numericUpDownQuantity = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxMaterials = new System.Windows.Forms.ComboBox();
+            this.listBoxParts = new System.Windows.Forms.ListBox();
+            this.pictureBoxMaterial = new System.Windows.Forms.PictureBox();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtOrder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuantity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMaterial)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownQuantity
+            // 
+            this.numericUpDownQuantity.Location = new System.Drawing.Point(35, 70);
+            this.numericUpDownQuantity.Name = "numericUpDownQuantity";
+            this.numericUpDownQuantity.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownQuantity.TabIndex = 1;
+            // 
+            // comboBoxMaterials
+            // 
+            this.comboBoxMaterials.FormattingEnabled = true;
+            this.comboBoxMaterials.Location = new System.Drawing.Point(34, 148);
+            this.comboBoxMaterials.Name = "comboBoxMaterials";
+            this.comboBoxMaterials.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMaterials.TabIndex = 2;
+            this.comboBoxMaterials.SelectedIndexChanged += new System.EventHandler(this.comboBoxMaterials_SelectedIndexChanged);
+            // 
+            // listBoxParts
+            // 
+            this.listBoxParts.FormattingEnabled = true;
+            this.listBoxParts.Location = new System.Drawing.Point(35, 189);
+            this.listBoxParts.Name = "listBoxParts";
+            this.listBoxParts.Size = new System.Drawing.Size(120, 95);
+            this.listBoxParts.TabIndex = 3;
+            this.listBoxParts.SelectedIndexChanged += new System.EventHandler(this.listBoxParts_SelectedIndexChanged);
+            // 
+            // pictureBoxMaterial
+            // 
+            this.pictureBoxMaterial.Location = new System.Drawing.Point(276, 132);
+            this.pictureBoxMaterial.Name = "pictureBoxMaterial";
+            this.pictureBoxMaterial.Size = new System.Drawing.Size(100, 50);
+            this.pictureBoxMaterial.TabIndex = 4;
+            this.pictureBoxMaterial.TabStop = false;
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.Location = new System.Drawing.Point(35, 316);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(75, 23);
+            this.btnOrder.TabIndex = 5;
+            this.btnOrder.Text = "Оформить";
+            this.btnOrder.UseVisualStyleBackColor = true;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(139, 316);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(273, 54);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(109, 13);
+            this.lblDescription.TabIndex = 7;
+            this.lblDescription.Text = "Обновляет text бокс";
+            // 
+            // txtOrder
+            // 
+            this.txtOrder.Location = new System.Drawing.Point(276, 70);
+            this.txtOrder.Name = "txtOrder";
+            this.txtOrder.Size = new System.Drawing.Size(100, 20);
+            this.txtOrder.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Укажите количество";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Материаллы";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOrder);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnOrder);
+            this.Controls.Add(this.pictureBoxMaterial);
+            this.Controls.Add(this.listBoxParts);
+            this.Controls.Add(this.comboBoxMaterials);
+            this.Controls.Add(this.numericUpDownQuantity);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuantity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMaterial)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.NumericUpDown numericUpDownQuantity;
+        private System.Windows.Forms.ComboBox comboBoxMaterials;
+        private System.Windows.Forms.ListBox listBoxParts;
+        private System.Windows.Forms.PictureBox pictureBoxMaterial;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox txtOrder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
